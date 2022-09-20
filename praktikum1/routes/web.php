8b1e2df0b8c873790897b5c42d58f828e0f467b8
dimas <dimasww1984@gmail.com>
2022-09-20 12:53:29 +0700

    migrate

--- praktikum1/routes/web.php
+++ praktikum1/routes/web.php
@@ -47,6 +47,21 @@ Route::get('users/{id}', function ($id) {
 
 // Route::get('/dosen','DosenController@index'); versi laravel 5
 
-Route::get('/', [App\Http\Controllers\DosenController::class,'index']);
+// Route::get('/', [App\Http\Controllers\DosenController::class,'index']);
 Route::get('/mahasiswa', [App\Http\Controllers\DosenController::class,'tampil']);
 Route::get('/biodata', [App\Http\Controllers\DosenController::class,'biodata']);
+
+// praktikum model
+Route::get('/', [App\Http\Controllers\MahasiswaController::class,'index']);
+Route::get('/insert-sql', [App\Http\Controllers\MahasiswaController::class,'insertSql']);
+Route::get('/insert-timestamp', [App\Http\Controllers\MahasiswaController::class,'insertTimestamp']);
+Route::get('/insert-prepared', [App\Http\Controllers\MahasiswaController::class,'insertPrepared']);
+Route::get('/insert-binding', [App\Http\Controllers\MahasiswaController::class,'insertBinding']);
+Route::get('/update', [App\Http\Controllers\MahasiswaController::class,'update']);
+Route::get('/delete', [App\Http\Controllers\MahasiswaController::class,'delete']);
+Route::get('/select', [App\Http\Controllers\MahasiswaController::class,'select']);
+Route::get('/select-tampil', [App\Http\Controllers\MahasiswaController::class,'selectTampil']);
+Route::get('/select-view', [App\Http\Controllers\MahasiswaController::class,'selectView']);
+Route::get('/select-where', [App\Http\Controllers\MahasiswaController::class,'selectWhere']);
+Route::get('/statement', [App\Http\Controllers\MahasiswaController::class,'statement']);
+
